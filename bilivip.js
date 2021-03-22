e90e890d86701ac3ffdb698f3aff35dd632eaea7
倪钍 <nitu2003@126.com>
2021-03-22 18:51:53 +0800

    Update bilivip.js

--- bilivip.js
+++ bilivip.js
@@ -4,6 +4,8 @@
 // @supportURL   https://github.com/nitu2003/bilivip/issues
 // @version      0.1
 // @description  变成年度小会员！
+// @updateURL    https://cdn.jsdelivr.net/gh/nitu2003/bilivip@latest/bilivip.js
+// @downloadURL  https://cdn.jsdelivr.net/gh/nitu2003/bilivip@latest/bilivip.js
 // @author       Taskeren
 // @match        *://space.bilibili.com/*
 // @grant        none
